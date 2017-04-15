8fcb70dddf90264e7bdaec34e6906e76f1700f4f
SAMUEL VERNECK <nodezeirojs@gmail.com>
2017-04-14 22:02:02 -0300

    Add files via upload

---
+++ tools/py/Codificacao.py
@@ -0,0 +1,6 @@
+mensagem = input('Digite a mensagem > ')
+cifra = int(input('Digite o valor da cifra > '))
+
+for i in range(len(mensagem)):
+    print (chr(ord(mensagem[i]) + cifra), end='')
+print ('')
